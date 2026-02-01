b1fe0364bc805649b74ea69494d0007d53922ea4
ahaduzzamanapon <ahaduzzamanapon@gmail.com>
2026-02-01 11:16:29 +0600

    dfrgrt

--- application/modules/admin/models/Manage_Contact_model.php
+++ application/modules/admin/models/Manage_Contact_model.php
@@ -1,29 +1,38 @@
 <?php
-if (!defined('BASEPATH')) exit('No direct script access allowed');
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
-class Manage_contact_model extends CI_Model {
+class Manage_contact_model extends CI_Model
+{
 
-    public function __construct() {
+    public function __construct()
+    {
         parent::__construct();
     }
 
-    public function get_data() {
+    public function get_data()
+    {
         // count query
-        $this->db->select('*');
+        $this->db->select('contact_us.*, services.name as service_name, products_new.name as product_name');
         $this->db->from('contact_us');
+        $this->db->join('services', 'contact_us.service = services.id', 'left');
+        $this->db->join('products_new', 'contact_us.product = products_new.id', 'left');
+        $this->db->order_by('contact_us.id', 'DESC');
         $query = $this->db->get()->result();
 
         return $query;
     }
 
-    public function get_info($id) {
+    public function get_info($id)
+    {
         $query = $this->db->from('contact_us')
-                        ->where('id', $id)
-                        ->get()->row();
+            ->where('id', $id)
+            ->get()->row();
         return $query;
     }
 
-    function delete($id) {
+    function delete($id)
+    {
         // $img_path = 'testimonial_img/';
         $info = $this->get_info($id);
 
@@ -34,9 +43,9 @@ class Manage_contact_model extends CI_Model {
 
         $this->db->where('id', $id);
         $this->db->delete('contact_us');
-        
+
         return TRUE;
     }
-   
+
 
 }
